65667d21c091398add209cecfa547c4c72c8d838
Alessandro Spasiano <alessandrospasiano@snps.it>
2020-10-16 17:01:21 +0200

    sistema fake list news components

--- listnews/src/staging/java/it/alexs/newsapplistnews/di/FakeListNewsComponent.kt
+++ listnews/src/staging/java/it/alexs/newsapplistnews/di/FakeListNewsComponent.kt
@@ -1,17 +1,14 @@
 package it.alexs.newsapplistnews.di
 
 import dagger.Component
-import it.alexs.newsapp.di.TestApplicationComponent
 import it.alexs.newsappmvvm_library.scope.ModuleScope
 import it.alexs.newsappnews_ibrary.FakeNewsModule
 import it.alexs.newsappnews_library.remote.NewsApiService
 
 @ModuleScope
-@Component(dependencies = [TestApplicationComponent::class], modules = [FakeNewsModule::class])
+@Component(modules = [ListNewsViewModelModule::class, FakeNewsModule::class])
 interface FakeListNewsComponent {
-    @Component.Factory
-    interface Factory {
-        fun create(appCompat: TestApplicationComponent): FakeListNewsComponent
-    }
+
+
     fun provideNewsApiService(): NewsApiService
 }
